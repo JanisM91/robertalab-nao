1d180ad881412e8c7576a632389cf5d6ca296486
Janis <janis.mohr@hs-bochum.de>
2016-11-03 15:47:01 +0100

    generate the md5 from file instead from String; adaption to recent commit on naoconnect.java

--- connection/md5token.py
+++ connection/md5token.py
@@ -2,4 +2,4 @@ import hashlib
 
 #generatedToken needs to be input
 f = open("md5naotoken.txt","w")
-f.write(hashlib.md5(generatedToken).hexdigest())
+f.write(hashlib.md5(open("/~/token.py", 'rb').read()).hexdigest())
